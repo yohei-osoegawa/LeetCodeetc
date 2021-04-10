dce2a80c4b008a38da5f286d5bc4195eb399024f
knockonthedoorintosummer <knockonthedoorintosummer@gmail.com>
2021-04-10 19:23:33 +0900

    preorderの分割方法を見直し、helperを用いない方式へ修正

--- NodejsConsoleApp2/leetCode105.ts
+++ NodejsConsoleApp2/leetCode105.ts
@@ -13,35 +13,28 @@ class TreeNode {
 
 function buildTree(preorder: number[], inorder: number[]): TreeNode | null {
 
-    const buildTreeSub = (preorder: number[], inorder: number[]): { tree: TreeNode | null, nextPre: number[] } => {
-        // 根が存在しない場合はnullとする
-        if (preorder.length === 0) {
-            return { tree: null, nextPre: [] }
-        }
-        // SubTreeとして空配列が与えられている場合はpreorderを変化させずにただちにnullNodeを返す
-        if (inorder.length === 0) {
-            return { tree: null, nextPre: preorder };
-        }
-
-        const [nodeVal, ...nextLeftPreorder] = preorder;  // 分割代入を用いてhead::tail形式に分割する
-
-        // 右SubTreeと左SubTreeを表す配列へとinorderを分割する
-        // ※なお、これはhead,tailが空配列だった場合も正しく機能するが、numが見つからない場合は例外を発生させるはず
-        const splitArrayAt = (array: number[], num: number): { head: number[], tail: number[] } => {
-            const mid = array.indexOf(num);
-            return { head: array.slice(0, mid), tail: array.slice(mid + 1, array.length) };
-        }
-        const { head, tail } = splitArrayAt(inorder, nodeVal);
-
-        // 右SubTreeと左SubTreeの生成は再帰的に任せる
-        const { tree: leftSubTree, nextPre: nextRightPreorder } = buildTreeSub(nextLeftPreorder, head);
-        const { tree: rightSubTree, nextPre } = buildTreeSub(nextRightPreorder, tail);
-
-        return { tree: new TreeNode(nodeVal, leftSubTree, rightSubTree), nextPre };
+    // 根が存在しない場合はnullとする
+    if (preorder.length === 0) {
+        return null
     }
 
-    const { tree, ..._ } = buildTreeSub(preorder, inorder);
-    return tree;
+    // 分割代入を用いてhead::tail形式に分割する
+    const [nodeVal, ...nextPreorder] = preorder;  
+
+    // 右SubTreeと左SubTreeを表す配列へとinorderを分割する
+    // ※なお、これはhead,tailが空配列だった場合も正しく機能するが、numが見つからない場合は例外を発生させるはず
+    const splitArrayAt = (array: number[], num: number): { head: number[], tail: number[] } => {
+        const mid = array.indexOf(num);
+        return { head: array.slice(0, mid), tail: array.slice(mid + 1, array.length) };
+    }
+    const { head: nextLeftInorder, tail: nextRightInorder } = splitArrayAt(inorder, nodeVal);
+
+    // 右SubTreeと左SubTreeの要素数を用いてPreorderも分割する
+    const nextLeftPreorder = nextPreorder.slice(0, nextLeftInorder.length);
+    const nextRightPreorder = nextPreorder.slice(nextLeftInorder.length);
+
+    // 右SubTreeと左SubTreeの生成は再帰的に任せる
+    return new TreeNode(nodeVal, buildTree(nextLeftPreorder, nextLeftInorder), buildTree(nextRightPreorder, nextRightInorder));
 };
 
 // デバグ用

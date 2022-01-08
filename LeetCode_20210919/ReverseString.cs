3e2cb36047feba7412abe8a3a5905981627d97f9
knockonthedoorintosummer <knockonthedoorintosummer@gmail.com>
2022-01-08 13:55:18 +0900

    テストに合格する単純解実装

--- LeetCode_20210919/ReverseString.cs
+++ LeetCode_20210919/ReverseString.cs
@@ -5,7 +5,7 @@ namespace LeetCode_20210919 {
         public static void Execute(char[] s) {
             // メモリを気にしない単純解、Reverseは使わない
             
-            foreach (var (x,i) in s.Select((x,i)=>(x,i))) {
+            foreach (var (x,i) in s.Select((x,i)=>(x,i)).ToList()) {
                 s[s.Length - i - 1] = x;
             }
         }

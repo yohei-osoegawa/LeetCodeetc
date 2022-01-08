95977b1c4ab976f8bf752269bbc0e573b3e490f6
knockonthedoorintosummer <knockonthedoorintosummer@gmail.com>
2022-01-08 13:53:08 +0900

    ReverseStringの単純回答とテスト

---
+++ LeetCode_20210919/ReverseString.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+
+namespace LeetCode_20210919 {
+    public static class ReverseString {
+        public static void Execute(char[] s) {
+            // メモリを気にしない単純解、Reverseは使わない
+            
+            foreach (var (x,i) in s.Select((x,i)=>(x,i))) {
+                s[s.Length - i - 1] = x;
+            }
+        }
+    }
+}
---
+++ LeetCode_SandBoxTests/ReverseStringTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode_20210919;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentAssertions;
+
+namespace LeetCode_20210919.Tests {
+    [TestClass()]
+    public class ReverseStringTests {
+        [TestMethod()]
+        public void ExecuteTest() {
+            var s = new[] { 'h', 'e', 'l', 'l', 'o' };
+            var expected = new[] { 'o', 'l', 'l', 'e', 'h' };
+            ReverseString.Execute(s);
+            s.Should().BeEquivalentTo(expected);
+        }
+    }
+}

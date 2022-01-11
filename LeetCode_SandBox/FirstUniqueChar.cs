fcf715f1ddaaf2f40d967372f32c290a61cdf8d1
knockonthedoorintosummer <knockonthedoorintosummer@gmail.com>
2022-01-11 19:08:31 +0900

    文字列から最初のUniqueな文字のindexを取得する単純解。＋テスト。

---
+++ LeetCode_SandBox/FirstUniqueChar.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+// https://leetcode.com/explore/interview/card/top-interview-questions-easy/127/strings/881/
+namespace LeetCode_SandBox {
+    public static class FirstUniqueChar {
+        public static int Execute(string s) {
+            // とりあえずDictionaryを使った単純解
+            var countDic = new Dictionary<char, (int count, int index)>();
+            var no = 0;
+            foreach (var (c,i) in s.Select((c,i)=>(c,i))) {
+                countDic[c] = countDic.TryGetValue(c, out var pair)
+                    ? (pair.count + 1, pair.index)
+                    : (1, i);
+            }
+            return countDic.Where(x => x.Value.count == 1)
+                .OrderBy(x => x.Value.index)
+                .Select(x => x.Value.index as int?)
+                .FirstOrDefault() ?? -1;
+        }
+    }
+}
---
+++ LeetCode_SandBoxTests/FirstUniqueCharTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode_SandBox;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using FluentAssertions;
+
+namespace LeetCode_SandBox.Tests {
+    [TestClass()]
+    public class FirstUniqueCharTests {
+        [TestMethod()]
+        public void ExecuteTest() {
+            var inputs = new[] { "leetcode", "loveleetcode", "aabb" };
+            var expexteds = new[] { 0, 2, -1 };
+            foreach (var (input, expected) in inputs.Zip(expexteds)) {
+                FirstUniqueChar.Execute(input).Should().Be(expected, $"input:{input}");
+            }
+        }
+    }
+}

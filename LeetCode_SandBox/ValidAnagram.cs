e8c7badbf35e411902e718c0b94634cd13feaafc
knockonthedoorintosummer <knockonthedoorintosummer@gmail.com>
2022-01-17 19:00:34 +0900

    ValidAnagramの単純解実装

---
+++ LeetCode_SandBox/ValidAnagram.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_SandBox {
+    // https://leetcode.com/explore/interview/card/top-interview-questions-easy/127/strings/882/
+    public static class ValidAnagram {
+        public static bool IsAnagram(string s, string t) {
+            // グルーピングして全一致しているか確認すればいいだけ。
+            // ※値がprimitiveなほうが比較の時楽
+            var sDic = s.ToCharArray().ToLookup(x => x).ToDictionary(x => x.Key, x => x.Count());
+            var tDic = t.ToCharArray().ToLookup(x => x).ToDictionary(x => x.Key, x => x.Count());
+
+            bool same = true;
+            same = sDic.Count == tDic.Count;
+
+            foreach (var (key,value) in sDic) {
+                if(!tDic.TryGetValue(key,out var value2)) {
+                    same = false;
+                    break;
+                }
+                if(value != value2) {
+                    same = false;
+                    break;
+                }
+            }
+            return same;
+        }
+    }
+}
---
+++ LeetCode_SandBoxTests/ValidAnagramTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode_SandBox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+
+namespace LeetCode_SandBox.Tests {
+    [TestClass()]
+    public class ValidAnagramTests {
+        [TestMethod()]
+        public void IsAnagramTest() {
+            var inputs1 = new[] {
+                "anagram",
+                "rat"
+            };
+            var inputs2 = new[] {
+                "nagaram",
+                "car"
+            };
+            var expecteds = new[] { true, false };
+
+
+            foreach (var ((s,t),expected) in inputs1.Zip(inputs2).Zip(expecteds)) {
+                ValidAnagram.IsAnagram(s, t).Should().Be(expected);
+            }
+        }
+    }
+}

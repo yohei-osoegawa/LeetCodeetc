c841c1ab3269a25ee14d586a2ba8261521a776b1
knockonthedoorintosummer <knockonthedoorintosummer@gmail.com>
2022-01-18 18:57:38 +0900

    回文判定メソッドの追加

---
+++ LeetCode_SandBox/ValidPalindrome.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace LeetCode_SandBox {
+    public static class ValidPalindrome {
+        public static bool Execute(string s) {
+            // 正規表現でエスケープすべき文字列の削除ができそうなので、それが回文か判定する（大文字小文字は問わない）
+            // https://docs.microsoft.com/ja-jp/dotnet/standard/base-types/character-escapes-in-regular-expressions
+            var formated = Regex.Replace(s, "[^a-zA-Z0-9]", "");
+            bool result = true;
+            foreach (var (c1,c2) in formated.Reverse().Zip(formated.ToCharArray())) {
+                if(Char.ToUpper(c1) != Char.ToUpper(c2)) {
+                    result = false;
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ LeetCode_SandBoxTests/ValidPalindromeTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode_SandBox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+
+namespace LeetCode_SandBox.Tests {
+    [TestClass()]
+    public class ValidPalindromeTests {
+        [TestMethod()]
+        public void ExecuteTest() {
+            var inputs = new[] {
+                "A man, a plan, a canal: Panama",
+                "race a car",
+                " ",
+            };
+            var expecteds = new[] {
+                true,
+                false,
+                true,
+            };
+            foreach (var (input,expect) in inputs.Zip(expecteds)) {
+                ValidPalindrome.Execute(input).Should().Be(expect);
+            }
+            
+        }
+    }
+}

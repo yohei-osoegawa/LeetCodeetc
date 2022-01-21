eb28f0d9bcac1486ab63a2ef6c692a2c851abcf3
knockonthedoorintosummer <knockonthedoorintosummer@gmail.com>
2022-01-21 19:42:08 +0900

    atoiメソッド作成。 バグが多発したのでテスト多め。

---
+++ LeetCode_SandBox/StringToInteger.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace LeetCode_SandBox {
+    public static class StringToInteger {
+        public static int MyAtoi(string s) {
+            // 正規表現かなあ
+            var resultCollection = Regex.Matches(s, @"(?<=^\s*)[+,-]?[0-9]+");
+            var str = resultCollection.FirstOrDefault()?.Value;
+            if (string.IsNullOrEmpty(str)) {
+                return 0;
+            }
+            if (int.TryParse(str, out var result)) {
+                return result;
+            } else if (str[0] == '-') {
+                return int.MinValue;
+            } else {
+                return int.MaxValue;
+            }
+        }
+    }
+}
---
+++ LeetCode_SandBoxTests/StringToIntegerTests.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode_SandBox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+
+namespace LeetCode_SandBox.Tests {
+    [TestClass()]
+    public class StringToIntegerTests {
+        [TestMethod()]
+        public void MyAtoiTest() {
+            var input = "42";
+            var expected = 42;
+            StringToInteger.MyAtoi(input).Should().Be(expected);
+        }
+
+        [TestMethod()]
+        public void MyAtoiTest2() {
+            var input = "   -42";
+            var expected = -42;
+            StringToInteger.MyAtoi(input).Should().Be(expected);
+        }
+
+        [TestMethod()]
+        public void MyAtoiTest3() {
+            var input = "4193 with words";
+            var expected = 4193;
+            StringToInteger.MyAtoi(input).Should().Be(expected);
+        }
+
+        [TestMethod()]
+        public void MyAtoiTest4() {
+            var input = "words and 987";
+            var expected = 0;
+            StringToInteger.MyAtoi(input).Should().Be(expected);
+        }
+
+
+        [TestMethod()]
+        public void MyAtoiTest5() {
+            var input = "-91283472332";
+            var expected = int.MinValue;
+            StringToInteger.MyAtoi(input).Should().Be(expected);
+        }
+
+        [TestMethod()]
+        public void MyAtoiTest6() {
+            var input = "90000000000";
+            var expected = int.MaxValue;
+            StringToInteger.MyAtoi(input).Should().Be(expected);
+        }
+    }
+}

cc49f753061672c0d615dcf97144a051b1bf4f9e
knockonthedoorintosummer <knockonthedoorintosummer@gmail.com>
2022-01-26 19:01:08 +0900

    ImprementstrStr実装。 ジェネレーターを使ってるからメモリも抑えられてると思う。

---
+++ LeetCode_SandBox/ImplementstrStr.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_SandBox {
+    // https://leetcode.com/explore/interview/card/top-interview-questions-easy/127/strings/885/
+    public static class ImplementstrStr {
+        public static int StrStr(string haystack, string needle) {
+            // window関数があれば一発
+            // CSharpには存在しないのでWindow関数を作って適用する。
+            static IEnumerable<string> StrWindow(string input, int length) {
+                if (length > input.Length) {
+                    yield return input ;
+                    yield break;
+                }
+                for (int i = 0; i < input.Length - length + 1; i++) {
+                    yield return input[i..(i+length)];
+                }
+            }
+
+            if(needle is "") {
+                return 0;
+            }
+
+            var windowed = StrWindow(haystack, needle.Length);
+            var cand = windowed.Select((value, i) => (value, i) ).FirstOrDefault(x=>x.value==needle);
+            if(cand == default) {
+                return -1;
+            } else {
+                return cand.i;
+            }
+
+        }
+    }
+}
---
+++ LeetCode_SandBoxTests/ImplementstrStrTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode_SandBox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+
+namespace LeetCode_SandBox.Tests {
+    [TestClass()]
+    public class ImplementstrStrTests {
+        [TestMethod()]
+        public void StrStrTest() {
+            var input1 = "hello";
+            var input2 = "ll";
+            var expected = 2;
+            ImplementstrStr.StrStr(input1, input2).Should().Be(expected);
+        }
+
+        [TestMethod()]
+        public void StrStrTest2() {
+            var input1 = "aaaaa";
+            var input2 = "bba";
+            var expected = -1;
+            ImplementstrStr.StrStr(input1, input2).Should().Be(expected);
+        }
+
+        [TestMethod()]
+        public void StrStrTest3() {
+            var input1 = "";
+            var input2 = "";
+            var expected = 0;
+            ImplementstrStr.StrStr(input1, input2).Should().Be(expected);
+        }
+    }
+}

117c2a9736ebd84ab05ef0b7472f1f81d85971dd
knockonthedoorintosummer <knockonthedoorintosummer@gmail.com>
2022-02-02 06:47:11 +0900

    最大長接頭辞の判別関数作成

---
+++ LeetCode_SandBox/LongestCommonPrefix.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_SandBox {
+    public static class LongestCommonPrefix {
+        public static string Execute(string[] strs) {
+            // 単なる総当たりで問題ないと判断
+            // より早くできる様子。どのように工夫すればいい？
+
+            if (strs.Length == 0) {
+                return "";
+            }
+
+            var minStr = strs.Aggregate((acc, next) => acc.Length > next.Length ? next : acc);
+            string result="";
+            for (int i = 0; i < minStr.Length; i++) {
+                var head = strs.First()[0..(i+1)];
+                if (strs.Select(str => str[0..(i+1)]).All(x => x == head)) {
+                    result = head;
+                } else {
+                    break;
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ LeetCode_SandBoxTests/LongestCommonPrefixTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode_SandBox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+
+namespace LeetCode_SandBox.Tests {
+    [TestClass()]
+    public class LongestCommonPrefixTests {
+        [TestMethod()]
+        public void ExecuteTest() {
+            var strs = new[] { "flower", "flow", "flight" };
+            var expected = "fl";
+            LongestCommonPrefix.Execute(strs).Should().Be(expected);
+        }
+        [TestMethod()]
+        public void ExecuteTest2() {
+            var strs = new[] { "dog", "racecar", "car" };
+            var expected = "";
+            LongestCommonPrefix.Execute(strs).Should().Be(expected);
+        }
+    }
+}

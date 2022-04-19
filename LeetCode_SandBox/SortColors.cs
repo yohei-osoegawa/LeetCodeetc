2374b39552b3ede645cebe4a47e786a441988f9d
knockonthedoorintosummer <knockonthedoorintosummer@gmail.com>
2022-04-20 07:25:20 +0900

    実装とテスト完了

---
+++ LeetCode_SandBox/SortColors.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class ClsSortColors {
+    public void SortColors(int[] nums) {
+        // 3状態しかないことに着目して、先頭と末尾へ選択ソートすれば線形時間で終わる。
+        var tail0 = 0;
+        var head2 = nums.Length - 1;
+        int i = 0;
+        while (i < head2 + 1) { 
+            if (nums[i] == 0) {
+                (nums[i], nums[tail0]) = (nums[tail0],nums[i]);
+                tail0++;
+                i = Math.Max(i, tail0);
+            } else if (nums[i] == 2) {
+                (nums[i], nums[head2]) = (nums[head2], nums[i]);
+                head2--;
+            } else {
+                i++;
+            }
+        }
+    }
+}
---
+++ LeetCode_SandBoxTests/ClsSortColorsTests.cs
@@ -0,0 +1,30 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Text.Json;
+
+namespace Tests {
+    [TestClass()]
+    public class ClsSortColorsTests {
+        [TestMethod()]
+        public void SortColorsTest() {
+            var jsonInp = "[2,0,2,1,1,0]";
+            var input = JsonSerializer.Deserialize<int[]>(jsonInp);
+
+            var expected = JsonSerializer.Deserialize<int[]>("[0,0,1,1,2,2]");
+            new ClsSortColors().SortColors(input);
+
+            input.Should().BeEquivalentTo(expected, option => option.WithStrictOrdering());
+        }
+
+        [TestMethod()]
+        public void SortColorsTest2() {
+            var jsonInp = "[2,0,1]";
+            var input = JsonSerializer.Deserialize<int[]>(jsonInp);
+
+            var expected = JsonSerializer.Deserialize<int[]>("[0,1,2]");
+            new ClsSortColors().SortColors(input);
+
+            input.Should().BeEquivalentTo(expected, option => option.WithStrictOrdering());
+        }
+    }
+}

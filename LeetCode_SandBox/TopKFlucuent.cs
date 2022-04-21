48d56d50dd908ca53e3e5490b4355be25d288a33
knockonthedoorintosummer <knockonthedoorintosummer@gmail.com>
2022-04-22 06:44:54 +0900

    最頻度数を求める処理を追加

---
+++ LeetCode_SandBox/TopKFlucuent.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq;
+
+// https://leetcode.com/explore/interview/card/top-interview-questions-medium/110/sorting-and-searching/799/
+public class ClsTopKFlucuent {
+    public int[] TopKFrequent(int[] nums, int k) {
+        return nums.ToLookup(x => x)
+            .Select(x => (value: x.Key, count: x.Count()))
+            .OrderByDescending(x => x.count)
+            .Select(x=>x.value)
+            .Take(k)
+            .ToArray();
+    }
+}
+
---
+++ LeetCode_SandBoxTests/ClsTopKFlucuentTests.cs
@@ -0,0 +1,16 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace Tests {
+    [TestClass()]
+    public class ClsTopKFlucuentTests {
+        [TestMethod()]
+        public void TopKFrequentTest() {
+            var nums = new[] { 1, 1, 1, 2, 2, 3 };
+            var k = 2;
+            var expected = new[] { 1, 2 };
+            new ClsTopKFlucuent().TopKFrequent(nums, 2).Should().BeEquivalentTo(expected);
+        }
+    }
+}

141d2c174a05a1127c2a08a25f7a1b1048c59255
knockonthedoorintosummer <knockonthedoorintosummer@gmail.com>
2022-04-25 07:02:26 +0900

    ニュートン法による局所的最大値を取得するロジック作成

---
+++ LeetCode_SandBox/ClsFindPeakElement.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class ClsFindPeakElement {
+    public int FindPeakElement(int[] nums) {
+        return FindPeakElement(nums, 0, nums.Length);
+    }
+
+    static int FindPeakElement(int[] nums, int begin, int end) {
+        if (end - begin == 1) {
+            return begin;
+        }
+        var halfi = (begin + end - 1) / 2;
+        if ((halfi == 0 || nums[halfi - 1] < nums[halfi])
+            && (halfi == nums.Length - 1 || nums[halfi] > nums[halfi + 1])
+        ) {
+            return halfi;
+        } else if (halfi == 0 || nums[halfi - 1] < nums[halfi]) {
+            return FindPeakElement(nums, halfi + 1, end);
+        } else {
+            return FindPeakElement(nums, begin, halfi + 1);
+        }
+    }
+}
+
---
+++ LeetCode_SandBoxTests/ClsFindPeakElementTests.cs
@@ -0,0 +1,30 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests {
+    [TestClass()]
+    public class ClsFindPeakElementTests {
+        [TestMethod()]
+        public void FindPeakElementTest() {
+            var nums = new[] { 1, 2, 1, 3, 5, 6, 4 };
+            var expected = 1;
+            var expected2 = 5;
+
+            new ClsFindPeakElement().FindPeakElement(nums)
+                .Should().Match(actual => actual == expected || actual == expected2);
+        }
+        [TestMethod()]
+        public void FindPeakElementTest2() {
+            var nums = new[] { 1, 2 };
+            var expected = 1;
+
+            new ClsFindPeakElement().FindPeakElement(nums)
+                .Should().Be(expected);
+        }
+    }
+}

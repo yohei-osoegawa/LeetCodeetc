8c6a988685eaf1b519aaa396ecfd9d97641064aa
knockonthedoorintosummer <knockonthedoorintosummer@gmail.com>
2022-04-24 14:09:18 +0900

    クイックソートによる、n次最大数の産出

---
+++ LeetCode_SandBox/KthLargestElementInArray.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// https://leetcode.com/explore/interview/card/top-interview-questions-medium/110/sorting-and-searching/800/
+public class KthLargestElementInArray {
+    public int FindKthLargest(int[] nums, int k) {
+        // クイックソートする
+        static void QuickSort(int[] nums, int begin, int end) {
+            if (end - begin <= 1) {
+                return;
+            } else if (end - begin == 2) {
+                if (nums[begin] > nums[end - 1]) {
+                    (nums[begin], nums[end - 1]) = (nums[end - 1], nums[begin]);
+                }
+                return;
+            }
+            var pivot = nums[(begin + end - 1) / 2];
+
+            var lefti = begin;
+            var righti = end - 1;
+            while (lefti <= righti) {
+                while (lefti <= righti && nums[lefti] <= pivot) {
+                    lefti++;
+                }
+                while (lefti <= righti && nums[righti] > pivot) {
+                    righti--;
+                }
+                if (lefti < righti) {
+                    (nums[lefti], nums[righti]) = (nums[righti], nums[lefti]);
+                } else if (lefti == end) {
+                    (nums[(begin + end - 1) / 2], nums[end - 1]) = (nums[end - 1], nums[(begin + end - 1) / 2]);
+                    righti = end - 2;
+                    break;
+                }
+            }
+            var end1 = righti;
+            QuickSort(nums, begin, end1 + 1);
+            QuickSort(nums, end1 + 1, end);
+        }
+        QuickSort(nums, 0, nums.Length);
+        return nums[^k];
+    }
+}
+
---
+++ LeetCode_SandBoxTests/KthLargestElementInArrayTests.cs
@@ -0,0 +1,45 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests {
+    [TestClass()]
+    public class KthLargestElementInArrayTests {
+        [TestMethod()]
+        public void FindKthLargestTest() {
+            var nums = new[] { 3, 2, 1, 5, 6, 4 };
+            var k = 2;
+
+            var expected = 5;
+            var actual = new KthLargestElementInArray().FindKthLargest(nums, k);
+
+            actual.Should().Be(expected);
+        }
+
+        [TestMethod()]
+        public void FindKthLargestTest2() {
+            var nums = new[] { 3, 2, 3, 1, 2, 4, 5, 5, 6 };
+            var k = 4;
+
+            var expected = 4;
+            var actual = new KthLargestElementInArray().FindKthLargest(nums, k);
+
+            actual.Should().Be(expected);
+        }
+
+        [TestMethod()]
+        public void FindKthLargestTest3() {
+            var nums = new[] { 5, 2, 4, 1, 3, 6, 0 };
+            var k = 2;
+
+            var expected = 5;
+            var actual = new KthLargestElementInArray().FindKthLargest(nums, k);
+
+            actual.Should().Be(expected);
+        }
+    }
+}

5d5b66e6ec0aade6c8ba96f9d1bd6a29d6763002
knockonthedoorintosummer <knockonthedoorintosummer@gmail.com>
2022-05-06 18:53:04 +0900

    配列の重複排除アルゴリズムとそのテストの作成

---
+++ LeetCode_SandBox/RemoveDuplicatedFromSortedArray.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/727/
+public class RemoveDuplicatedFromSortedArray {
+    public int RemoveDuplicates(int[] nums) {
+        int? acc = null;
+        int count = 0;
+        int dsti = 0;
+        for (int i = 0; i < nums.Length; i++) {
+            if (acc != nums[i]) {
+                acc = nums[dsti] = nums[i];
+                dsti++;
+                count++;
+            }
+        }
+        return count;
+    }
+}
+
---
+++ LeetCode_SandBoxTests/RemoveDuplicatedFromSortedArrayTests.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests {
+    [TestClass()]
+    public class RemoveDuplicatedFromSortedArrayTests {
+        [TestMethod()]
+        public void RemoveDuplicatesTest() {
+            var input = new[] { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 };
+            var expected = 5;
+            var actual = new RemoveDuplicatedFromSortedArray().RemoveDuplicates(input);
+            actual.Should().Be(expected);
+            input.Should().StartWith(new[] { 0, 1, 2, 3, 4 });
+        }
+    }
+}

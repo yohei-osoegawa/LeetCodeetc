b0dbe547750722a757e6f6bae0670d0946b4efe4
knockonthedoorintosummer <knockonthedoorintosummer@gmail.com>
2022-05-07 19:56:46 +0900

    配列のスピンとそのテスト作成

---
+++ LeetCode_SandBox/RotateArray.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class RotateArray {
+    public void Rotate(int[] nums, int k) {
+        // Hack: メモリ性能が悪い。タプルを用いたスワップのせい？
+        var length = nums.Length;
+        var tmp = nums[0];
+        int j = k % length;
+        var sheed = 0;
+        for (int i = 0; i < length ; i++) {
+            if (j == sheed) {
+                (nums[j], tmp) = (tmp, nums[j]);
+                sheed = (sheed + 1) % length;
+                j = (sheed + k) % length;
+                tmp = nums[sheed];
+            }
+            else{
+                (nums[j], tmp) = (tmp, nums[j]);
+                j = (j + k) % length;
+            }
+        }
+    }
+}
---
+++ LeetCode_SandBoxTests/RotateArrayTests.cs
@@ -0,0 +1,30 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests {
+    [TestClass()]
+    public class RotateArrayTests {
+        [TestMethod()]
+        public void RotateTest() {
+            var nums = new[] { 1, 2, 3, 4, 5, 6, 7 };
+            var k = 3;
+            new RotateArray().Rotate(nums, k);
+            var expected = new[] { 5, 6, 7, 1, 2, 3, 4 };
+            nums.Should().Equal(expected);
+        }
+
+        [TestMethod()]
+        public void RotateTest2() {
+            var nums = new[] { -1, -100, 3, 99 };
+            var k = 2;
+            new RotateArray().Rotate(nums, k);
+            var expected = new[] { 3, 99, -1, -100 };
+            nums.Should().Equal(expected);
+        }
+    }
+}

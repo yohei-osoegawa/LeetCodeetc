39cb8b4dcca2bd8a80f22e5e0ec008475d976168
knockonthedoorintosummer <knockonthedoorintosummer@gmail.com>
2022-05-13 20:04:40 +0900

    差分の正数のみの和を生成、およびそのテスト

---
+++ LeetCode_SandBox/BestTimeToBuyStock.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class BestTimeToBuyStock {
+    public int MaxProfit(int[] prices) {
+        return prices.Aggregate(
+            (result: new List<int>(), before: null as int?),
+            (acc, x) => {
+                if (acc.before is not int num) {
+                    return (acc.result, x);
+                } else {
+                    acc.result.Add(x - num);
+                    return (acc.result, x);
+                }
+            },
+            x => x.result // 差分リストの生成
+        ).Where(x => x > 0)
+            .Cast<int?>()
+            .Sum() ?? 0;
+    }
+}
+
---
+++ LeetCode_SandBoxTests/BestTimeToBuyStockTests.cs
@@ -0,0 +1,28 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests {
+    [TestClass()]
+    public class BestTimeToBuyStockTests {
+        [TestMethod()]
+        public void MaxProfitTest() {
+            var input = new[] { 7, 1, 5, 3, 6, 4 };
+            var expected = 7;
+            var actual = new BestTimeToBuyStock().MaxProfit(input);
+            actual.Should().Be(expected);
+        }
+
+        [TestMethod()]
+        public void MaxProfitTest2() {
+            var input = new[] { 7, 6, 4, 3, 1 };
+            var expected = 0;
+            var actual = new BestTimeToBuyStock().MaxProfit(input);
+            actual.Should().Be(expected);
+        }
+    }
+}

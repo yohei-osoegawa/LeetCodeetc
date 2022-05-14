52e6e541bf50f1ee54813b1a6a6b48a51a16676c
knockonthedoorintosummer <knockonthedoorintosummer@gmail.com>
2022-05-14 21:13:48 +0900

    XORを用いた偶数個重複の排除

---
+++ LeetCode_SandBox/SingleNumber.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class ClsSingleNumber {
+    public int SingleNumber(int[] nums) {
+        // ビットXOR演算を用いると偶数個数のものが消去される。
+        return nums.Aggregate((acc, x) => acc ^ x);
+    }
+}
+
---
+++ LeetCode_SandBoxTests/ClsSingleNumberTests.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests {
+    [TestClass()]
+    public class ClsSingleNumberTests {
+        [TestMethod()]
+        public void SingleNumberTest() {
+            var nums = new[] { 4, 1, 2, 1, 2 };
+            var expected = 4;
+            new ClsSingleNumber().SingleNumber(nums).Should().Be(expected);
+        }
+    }
+}

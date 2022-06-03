cf5411a7109015081511c230cba9e3fec059f28e
knockonthedoorintosummer <knockonthedoorintosummer@gmail.com>
2022-06-03 19:40:47 +0900

    LinkedListのNode消去実装

---
+++ LeetCode_SandBox/DeleteNodeInALinkedList.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class DeleteNodeInALinkedList {
+    public void DeleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+}
+
+public class ListNode {
+    public int val;
+    public ListNode next;
+    public ListNode(int x) { val = x; }
+}
